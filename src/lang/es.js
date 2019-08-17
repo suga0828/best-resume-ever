5201eac0e782730271d20b13a10432045570db9e
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-08-17 06:30:38 -0500

    Fix 'born' and 'bornIn' in spanish.

--- src/lang/es.js
+++ src/lang/es.js
@@ -6,6 +6,8 @@ const lang = {
     skills: 'Habilidades',
     projects: 'Proyectos',
     contributions: 'Contribuciones',
+    born: 'Nació en',
+    bornIn: 'en',
     about: 'Sobre mi'
 };
 export default lang;
